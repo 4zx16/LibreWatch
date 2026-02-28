50736ae0914d5a28ad2f41226e03b0263a42c824
ΣΝΛ <78288792+4zx16@users.noreply.github.com>
2026-02-28 22:16:46 +0000

    Update config.js
    Signed-off-by: ΣΝΛ <78288792+4zx16@users.noreply.github.com>

--- Player/config.js
+++ Player/config.js
@@ -1,8 +1,25 @@
 // config.js
 export const config = {
-    UI: "https://invidious.nerdvpn.de/", // This loads the frontend for watch together, displays all videos
-    sponsorBlock: "https://sponsor.ajay.app/", // Blocks all sponsors
-    dearrow: "https://dearrow.ajay.app/", // Shows nonclick titles
-    API: "https://invidious.kavin.rocks/api/v1/", // Plays videos & fetches them
-   // Proxy: "https://cors-anywhere.herokuapp.com/"  // Bypass CORS errors and resolves them
+  UI: {
+    Invidious: {
+      "NerdVPN": "https://invidious.nerdvpn.de/",
+      "NerdVPN (Onion)": "http://nerdvpneaggggfdiurknszkbmhvjndks5z5k3g5yp4nhphflh3n3boad.onion/",
+      "Nadeko (I2P)": "http://nadekoohummkxncchcsylr3eku36ze4waq4kdrhcqupckc3pe5qq.b32.i2p/",
+      "Nadeko": "https://inv.nadeko.net/"
+    },
+    Piped: {
+      "Piped": "https://piped.video/",
+      "Frontendprivacy": "https://pipedapi.frontendfriendly.xyz/",
+      "kavin.rocks": "https://pipedapi.kavin.rocks/"
+    }
+  },
+  sponsorBlock: "https://sponsor.ajay.app/",
+  dearrow: "https://dearrow.ajay.app/",
+  API: "https://invidious.kavin.rocks/api/v1/",
+  Proxy: {
+    CorsProxy: "https://corsproxy.io/?url=",
+    AllOriginsRaw: "https://api.allorigins.win/raw?url=",
+    AllOriginsJSON: "https://api.allorigins.win/get?url=",
+    CorsProxy2: "https://corsproxy.cyou/?url="
+  } // Bypass CORS
 };

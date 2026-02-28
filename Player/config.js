08479c93584edfae399546d43954c7d2ae8eb2e8
ΣΝΛ <78288792+4zx16@users.noreply.github.com>
2026-02-28 23:04:06 +0000

    Update config.js
    Signed-off-by: ΣΝΛ <78288792+4zx16@users.noreply.github.com>

--- Player/config.js
+++ Player/config.js
@@ -1,25 +1,32 @@
 // config.js
 export const config = {
-  UI: {
-    Invidious: {
-      "NerdVPN": "https://invidious.nerdvpn.de/",
-      "NerdVPN (Onion)": "http://nerdvpneaggggfdiurknszkbmhvjndks5z5k3g5yp4nhphflh3n3boad.onion/",
-      "Nadeko (I2P)": "http://nadekoohummkxncchcsylr3eku36ze4waq4kdrhcqupckc3pe5qq.b32.i2p/",
-      "Nadeko": "https://inv.nadeko.net/"
+  Player: {
+    UI: {
+      default: "https://www.youtube-nocookie.com/embed/",
+      Invidious: {
+        "NerdVPN": "https://invidious.nerdvpn.de/",
+        "NerdVPN (Onion)": "http://nerdvpneaggggfdiurknszkbmhvjndks5z5k3g5yp4nhphflh3n3boad.onion/",
+        "Nadeko (I2P)": "http://nadekoohummkxncchcsylr3eku36ze4waq4kdrhcqupckc3pe5qq.b32.i2p/",
+        "Nadeko": "https://inv.nadeko.net/"
+      },
+      Piped: {
+        "Piped": "https://piped.video/",
+        "Frontendprivacy": "https://pipedapi.frontendfriendly.xyz/",
+        "kavin.rocks": "https://pipedapi.kavin.rocks/"
+      }
     },
-    Piped: {
-      "Piped": "https://piped.video/",
-      "Frontendprivacy": "https://pipedapi.frontendfriendly.xyz/",
-      "kavin.rocks": "https://pipedapi.kavin.rocks/"
+    Misc: {
+      sponsorBlock: "https://sponsor.ajay.app/",
+      dearrow: "https://dearrow.ajay.app/"
+    },
+    Proxy: {
+      CorsProxy: "https://corsproxy.io/?url=",
+      AllOriginsRaw: "https://api.allorigins.win/raw?url=",
+      AllOriginsJSON: "https://api.allorigins.win/get?url=",
+      CorsProxy2: "https://corsproxy.cyou/?url="
+    },
+    APIs: {
+      API: "https://invidious.kavin.rocks/api/v1/"
     }
-  },
-  sponsorBlock: "https://sponsor.ajay.app/",
-  dearrow: "https://dearrow.ajay.app/",
-  API: "https://invidious.kavin.rocks/api/v1/",
-  Proxy: {
-    CorsProxy: "https://corsproxy.io/?url=",
-    AllOriginsRaw: "https://api.allorigins.win/raw?url=",
-    AllOriginsJSON: "https://api.allorigins.win/get?url=",
-    CorsProxy2: "https://corsproxy.cyou/?url="
-  } // Bypass CORS
+  }
 };

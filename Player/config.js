94d0906e9e7f2ae5d60a9a5bfe30c562b799c40c
ΣΝΛ <78288792+4zx16@users.noreply.github.com>
2026-03-01 03:31:36 +0000

    Update config.js
    Signed-off-by: ΣΝΛ <78288792+4zx16@users.noreply.github.com>

--- Player/config.js
+++ Player/config.js
@@ -4,20 +4,48 @@ export const config = {
     UI: {
       default: "https://www.youtube-nocookie.com/embed/",
       Invidious: {
-        "NerdVPN": "https://invidious.nerdvpn.de/embed/",
-        "NerdVPN (Onion)": "http://nerdvpneaggggfdiurknszkbmhvjndks5z5k3g5yp4nhphflh3n3boad.onion/embed/",
-        "Nadeko (I2P)": "http://nadekoohummkxncchcsylr3eku36ze4waq4kdrhcqupckc3pe5qq.b32.i2p/embed/",
+        "NerdVPN": "https://invidious.nerdvpn.de/",
+        "NerdVPN (Onion)": "http://nerdvpneaggggfdiurknszkbmhvjndks5z5k3g5yp4nhphflh3n3boad.onion/",
+        "Nadeko (I2P)": "http://nadekoohummkxncchcsylr3eku36ze4waq4kdrhcqupckc3pe5qq.b32.i2p/",
         "Nadeko": "https://inv.nadeko.net/"
       },
       Piped: {
-        "Piped": "https://piped.video/embed/",
-        "Frontendprivacy": "https://pipedapi.frontendfriendly.xyz/embed/",
-        "kavin.rocks": "https://pipedapi.kavin.rocks/embed/"
+        "Piped": "https://piped.video/",
+        "Frontendprivacy": "https://pipedapi.frontendfriendly.xyz/",
+        "kavin.rocks": "https://pipedapi.kavin.rocks/"
       }
     },
     Misc: {
-      sponsorBlock: "https://sponsor.ajay.app/",
-      dearrow: "https://dearrow.ajay.app/"
+      sponsorBlock: {
+        API: "https://sponsor.ajay.app/",
+        //KEY: "",
+        config: {
+        Categories: {
+        sponsor: "skip",            // Paid promotions
+        selfpromo: "skip",          // "Check out my channel"
+        interaction: "skip",        // "Like and subscribe"
+        intro: "skip",              // Long intros
+        outro: "skip",              // Outros/end screens
+        preview: "skip",            // Preview recaps
+        filler: "skip",             // Filler content
+        //music_offtopic: "mute",     // Off-topic music
+        poi_highlight: "none"       // Highlight only
+        },
+      AutoSkip: true,
+      SkipThreshold: 0, // skip immediately
+      ShowToast: false
+        }
+      },
+      dearrow: {
+        API: "https://dearrow.ajay.app/",
+        KEY: "FR3Lo-e986a",
+        config: {
+        ReplaceTitles: true,
+        ReplaceThumbnails: true,
+        FallbackToOriginal: true,
+        PreferOriginalIfNoVotes: true
+        }
+      }
     },
     Proxy: {
       CorsProxy: "https://corsproxy.io/?url=",
@@ -26,7 +54,16 @@ export const config = {
       CorsProxy2: "https://corsproxy.cyou/?url="
     },
     APIs: {
-      API: "https://invidious.kavin.rocks/api/v1/"
+      Indivious: {
+        "Kavin.rocks API": "https://invidious.kavin.rocks/api/v1/"
+        // May not use APIs
+      },
+      Piped: {
+        // Not sure if I'm going to use APIs on this
+      },
+      Google: {
+        API_KEY: ""
+      } // Not used, may never be used, who knows.
     }
   }
 };
